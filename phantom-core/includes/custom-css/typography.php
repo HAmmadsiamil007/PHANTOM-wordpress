8afce1528dbe3fab857a9e2b542810f4b97f0479
HAmmadsiamil007 <hamadsiamil007@gmail.com>
2026-07-22 12:15:10 +0500

    feat: generate CSS for per-heading typography with inheritance
    - Added per-heading font/weight/style/spacing/case CSS generation
    - Inheritance chain: per-heading -> heading default -> body default
    - Added typography_body_style CSS var output
    - Added px suffix for body/heading spacing values

--- phantom-core/includes/custom-css/typography.php
+++ phantom-core/includes/custom-css/typography.php
@@ -12,11 +12,10 @@ add_filter(
 	function ( string $css ): string {
 		$map    = \PhantomCore\Settings_Registry::get_css_var_map();
 		$output = '';
-		$composite = get_option( 'phantom_typography_headings', array() );
 
 		$keys = array(
-			'typography_body_font', 'typography_body_weight', 'typography_base_size',
-			'typography_line_height', 'typography_body_spacing',
+			'typography_body_font', 'typography_body_weight', 'typography_body_style',
+			'typography_base_size', 'typography_line_height', 'typography_body_spacing',
 			'typography_heading_font', 'typography_heading_weight',
 			'typography_heading_case', 'typography_heading_spacing',
 			'typography_h1_size', 'typography_h1_height',
@@ -36,26 +35,55 @@ add_filter(
 			$val = get_option( 'phantom_' . $k, '' );
 			if ( '' !== $val ) {
 				$val_display = $val;
-				if ( in_array( $k, array( 'typography_base_size', 'menu_font_size' ), true ) && is_numeric( $val ) ) {
+				if ( in_array( $k, array( 'typography_base_size', 'menu_font_size', 'typography_body_spacing', 'typography_heading_spacing' ), true ) && is_numeric( $val ) ) {
 					$val_display .= 'px';
 				}
 				$output .= "\t" . $map[ $k ] . ': ' . esc_attr( $val_display ) . ';' . "\n";
 			}
 		}
 
-		if ( is_array( $composite ) ) {
-			$headings = array( 'h1', 'h2', 'h3', 'h4', 'h5', 'h6' );
-			foreach ( $headings as $h_tag ) {
-				if ( ! isset( $composite[ $h_tag ] ) ) {
-					continue;
+		$headings = array( 'h1', 'h2', 'h3', 'h4', 'h5', 'h6' );
+		foreach ( $headings as $h ) {
+			$prefix = 'typography_' . $h . '_';
+
+			$font = get_option( 'phantom_' . $prefix . 'font', '' );
+			if ( '' === $font ) {
+				$font = get_option( 'phantom_typography_heading_font', '' );
+			}
+			if ( '' === $font ) {
+				$font = get_option( 'phantom_typography_body_font', 'Archivo' );
+			}
+			if ( isset( $map[ $prefix . 'font' ] ) ) {
+				$output .= "\t" . $map[ $prefix . 'font' ] . ': ' . esc_attr( $font ) . ";\n";
+			}
+
+			$weight = get_option( 'phantom_' . $prefix . 'weight', '' );
+			if ( '' === $weight ) {
+				$weight = get_option( 'phantom_typography_heading_weight', '500' );
+			}
+			if ( isset( $map[ $prefix . 'weight' ] ) ) {
+				$output .= "\t" . $map[ $prefix . 'weight' ] . ': ' . esc_attr( $weight ) . ";\n";
+			}
+
+			if ( isset( $map[ $prefix . 'style' ] ) ) {
+				$style = get_option( 'phantom_' . $prefix . 'style', 'normal' );
+				$output .= "\t" . $map[ $prefix . 'style' ] . ': ' . esc_attr( $style ) . ";\n";
+			}
+
+			$spacing = get_option( 'phantom_' . $prefix . 'spacing', '' );
+			if ( '' === $spacing ) {
+				$spacing = get_option( 'phantom_typography_heading_spacing', '0' );
+			}
+			if ( isset( $map[ $prefix . 'spacing' ] ) ) {
+				$output .= "\t" . $map[ $prefix . 'spacing' ] . ': ' . esc_attr( floatval( $spacing ) ) . "px;\n";
+			}
+
+			if ( isset( $map[ $prefix . 'case' ] ) ) {
+				$case = get_option( 'phantom_' . $prefix . 'case', 'none' );
+				if ( 'none' === $case || '' === $case ) {
+					$case = get_option( 'phantom_typography_heading_case', 'none' );
 				}
-				$h_data = $composite[ $h_tag ];
-				if ( isset( $h_data['size'] ) && '' !== $h_data['size'] ) {
-					$var = $map[ 'typography_' . $h_tag . '_size' ] ?? null;
-					if ( $var ) {
-						$output .= "\t" . $var . ': ' . esc_attr( $h_data['size'] ) . ";\n";
-					}
-				}
+				$output .= "\t" . $map[ $prefix . 'case' ] . ': ' . esc_attr( $case ) . ";\n";
 			}
 		}
 
